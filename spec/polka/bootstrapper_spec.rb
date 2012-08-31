34750543cd5266499ffcaf3c8c99508006fea8f1
Michi Huber <michi.huber@gmail.com>
2012-08-31 17:37:10 +0200

    don't test bootstrapper in integration

--- spec/polka/bootstrapper_spec.rb
+++ spec/polka/bootstrapper_spec.rb
@@ -1,34 +1,43 @@
 require_relative "../../lib/polka/bootstrapper.rb"
-require_relative "../../lib/polka/dotfile.rb"
-require_relative "../../lib/polka/dotfile_group.rb"
+class Dotfile; end
+class DotfileGroup; end
 
 describe Polka::Bootstrapper do
   subject(:bs) { Polka::Bootstrapper.new('home', 'home/.polka') }
+  let(:symlink_group) { double(:symlinks) }
+  let(:excluded_group) { double(:excluded) }
+
+  before do
+    Dir.stub(:new) { double(entries: %w(. .. blah)) }
+    blah_dotfile = double
+    the_dotfile = double(:Dotfile)
+    Dotfile.should_receive(:new).with('home/.polka/blah', 'home/blah').and_return(blah_dotfile)
+    Dotfile.should_receive(:new).with('home/.polka/Dotfile', 'home/Dotfile').and_return(the_dotfile)
+    excluded_group.should_receive(:add).with([the_dotfile])
+    DotfileGroup.should_receive(:all_files=).with([blah_dotfile])
+    DotfileGroup.stub(:new).and_return(symlink_group, excluded_group)
+  end
 
   describe "#setup" do
-    let(:dir) { double(entries: %w(. .. .norc .badrc .onerc .tworc .threerc)) }
-    before do
-      Dir.stub(:new) { dir }
-    end
-
-    it "symlinks the specified files" do
-      bs.symlink('.onerc')
-      FileUtils.should_receive(:ln_s).with("home/.polka/.onerc", "home/.onerc")
+    it "sets up the symlink group" do
+      symlink_group.should_receive(:setup)
       bs.setup
     end
+  end
 
-    context "if :all_other_files are used" do
-      it "symlinks all files except excluded" do
-        bs.exclude('.badrc')
-        bs.symlink('.onerc')
-        bs.symlink(:all_other_files)
-        bs.symlink('.threerc')
-        bs.exclude('.norc')
-        FileUtils.should_receive(:ln_s).with("home/.polka/.onerc", "home/.onerc")
-        FileUtils.should_receive(:ln_s).with("home/.polka/.tworc", "home/.tworc")
-        FileUtils.should_receive(:ln_s).with("home/.polka/.threerc", "home/.threerc")
-        bs.setup
-      end
+  describe "#symlink, #exclude" do
+    it "adds to the symlink group" do
+      hello_dotfile = double(:hello_dotfile)
+      Dotfile.stub(:new) { hello_dotfile }
+      symlink_group.should_receive(:add).with([hello_dotfile])
+      bs.symlink "hello"
+    end
+
+    it "adds to the exclude group" do
+      hello_dotfile = double(:hello_dotfile)
+      Dotfile.stub(:new) { hello_dotfile }
+      excluded_group.should_receive(:add).with([hello_dotfile])
+      bs.exclude "hello"
     end
   end
 end
