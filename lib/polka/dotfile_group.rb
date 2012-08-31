1f543d302f12ecd16b650842026b1aa8cde1b5c8
Michi Huber <michi.huber@gmail.com>
2012-08-31 14:24:29 +0200

    raise errors if the same file or :all_other_files is added multiple times

--- lib/polka/dotfile_group.rb
+++ lib/polka/dotfile_group.rb
@@ -1,9 +1,15 @@
+class AllOtherFilesAlreadyAdded < StandardError; end
+class FileAlreadyAddedError < StandardError; end
+
 class DotfileGroup
   @@all_files = []
   @@grouped_files = []
+  @@all_other_files_added = false
+
   def self.all_files=(files)
     @@all_files = files
     @@grouped_files = []
+    @@all_other_files_added = false
   end
 
   def self.remaining_files
@@ -16,11 +22,14 @@ class DotfileGroup
   end
 
   def add(new_files)
+    raise FileAlreadyAddedError if new_files.any? { |f| @@grouped_files.include?(f) }
     @@grouped_files += new_files
     @files += new_files
   end
 
   def add_all_other_files
+    raise AllOtherFilesAlreadyAdded if @@all_other_files_added
+    @@all_other_files_added = true
     @all_other_files = true
   end
 
--- spec/polka/dotfile_group_spec.rb
+++ spec/polka/dotfile_group_spec.rb
@@ -2,22 +2,37 @@ require_relative "../../lib/polka/dotfile_group.rb"
 
 describe DotfileGroup do
   subject(:group) { DotfileGroup.new }
+  1.upto(3) { |n| fn = "file#{n}".to_sym; let(fn) { double(fn) } }
+
+  before do
+    DotfileGroup.all_files = [file1, file2, file3]
+  end
 
   describe "#add" do
     it "adds dotfiles" do
-      group.add([1,2,3])
-      group.files.should == [1,2,3]
+      group.add([file1, file2])
+      group.files.should == [file1, file2]
+    end
+
+    it "can only add each file once" do
+      group.add([file1])
+      expect { group.add([file1]) }.to raise_error(FileAlreadyAddedError)
     end
   end
 
   describe "#add_all_other_files" do
+    let(:other_group) { DotfileGroup.new }
+
     it "adds all other files to the group" do
-      DotfileGroup.all_files = [1, 2, 3]
-      other_group = DotfileGroup.new
-      group.add([1])
+      group.add([file1])
       group.add_all_other_files
-      other_group.add([3])
-      group.files.sort.should == [1,2]
+      other_group.add([file3])
+      group.files.should == [file2, file1]
+    end
+
+    it "can only be called once" do
+      group.add_all_other_files
+      expect {other_group.add_all_other_files }.to raise_error(AllOtherFilesAlreadyAdded)
     end
   end
 end
