fa48f18bcc998e953798554c40fe7e75bf9f89c8
Michi Huber <michi.huber@gmail.com>
2012-09-01 17:08:54 +0200

    expand path on setup

--- lib/polka/cli.rb
+++ lib/polka/cli.rb
@@ -4,6 +4,8 @@ module Polka
     def setup(home_dir=nil, dotfile_dir=nil)
       dotfile_path = dotfile_dir ? File.join(dotfile_dir, "Dotfile") : "Dotfile"
       home_path = home_dir || ENV['HOME']
+      dotfile_path = File.expand_path(dotfile_path)
+      home_path = File.expand_path(home_path)
       File.open(dotfile_path) { |the_dotfile| Bootstrapper.from_dotfile(the_dotfile, home_path).setup }
     end
   end
--- spec/polka/cli_spec.rb
+++ spec/polka/cli_spec.rb
@@ -8,16 +8,16 @@ describe Polka::Cli do
     let(:bootstrapper) { double(:bootstrapper, setup: "works") }
 
     it "initializes a bootstrapper with the Dotfile (and the target dir)" do
-      File.should_receive(:open).with("Dotfile").and_yield(the_dotfile)
-      Polka::Bootstrapper.should_receive(:from_dotfile).with(the_dotfile, ENV['HOME']).and_return(bootstrapper)
+      File.stub(:open).and_yield(the_dotfile)
+      Polka::Bootstrapper.should_receive(:from_dotfile).with(the_dotfile, File.expand_path(ENV['HOME'])).and_return(bootstrapper)
       bootstrapper.setup.should == "works"
 
       Polka::Cli.new.setup
     end
 
     it "accepts home_dir and dotfile_dir as optional arguments" do
-      File.should_receive(:open).with("home/.polka/Dotfile").and_yield(the_dotfile)
-      Polka::Bootstrapper.should_receive(:from_dotfile).with(the_dotfile, "home").and_return(bootstrapper)
+      File.stub(:open).and_yield(the_dotfile)
+      Polka::Bootstrapper.should_receive(:from_dotfile).with(the_dotfile, File.expand_path("home")).and_return(bootstrapper)
       bootstrapper.setup.should == "works"
 
       Polka::Cli.new.setup("home", "home/.polka")
